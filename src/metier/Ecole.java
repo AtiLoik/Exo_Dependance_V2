40b0b0b09f367397d67d12385e1463ef1f09c9db
loik froelich <loikfr@live.fr>
2019-12-20 14:44:05 +0100

    Ajout du CRUD dans Ecole

--- src/metier/Ecole.java
+++ src/metier/Ecole.java
@@ -1,6 +1,7 @@
 package metier;
 
-import java.util.ArrayList;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 public class Ecole {
@@ -43,5 +44,143 @@ public class Ecole {
 	@Override
 	public String toString() {
 		return "Ecole [nomEcole=" + nomEcole + ", adresse=" + adresseEcole + "]";
+	}
+	
+	public static void sauverEcole(int nomEcole, String adresseEcole)
+	{
+		String url = "jdbc:mysql://localhost/formation";	// Changer nom formation
+		String login = "root";
+		String passwd = "";
+		java.sql.Connection cn = null;
+		java.sql.Statement st = null;
+		java.sql.ResultSet rs = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			cn = DriverManager.getConnection(url, login, passwd);
+			st = cn.createStatement();
+			String sql = "Insert into Ecole values ("+nomEcole+", '"+ adresseEcole +"')";
+			st.executeUpdate(sql);
+			sql = "Select * from departement";
+			rs = st.executeQuery(sql);
+			while(rs.next()) {
+				String res = rs.getString("id")+ " " + rs.getString("nom") ;
+				System.out.println(res);
+			}
+		}
+		catch (SQLException e ) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				cn.close();
+				st.close();
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void listeEcole()
+	{
+		String url = "jdbc:mysql://localhost/formation";	// Changer nom formation
+		String login = "root";
+		String passwd = "";
+		java.sql.Connection cn = null;
+		java.sql.Statement st = null;
+		java.sql.ResultSet rs = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			cn = DriverManager.getConnection(url, login, passwd);
+			st = cn.createStatement();
+			String sql = "Select * from Ecole";
+			rs = st.executeQuery(sql);
+			while(rs.next()) {
+				String res = rs.getString("nomEcole")+ " " + rs.getString("adresseEcole") ;
+				System.out.println(res);
+			}
+		}
+		catch (SQLException e ) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				cn.close();
+				st.close();
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void supprimerEcole(String nomEcole)
+	{
+		String url = "jdbc:mysql://localhost/formation";
+		String login = "root";
+		String passwd = "";
+		java.sql.Connection cn = null;
+		java.sql.Statement st = null;
+		java.sql.ResultSet rs = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			cn = DriverManager.getConnection(url, login, passwd);
+			st = cn.createStatement();
+			String sql = "delete from Ecole where nomEcole ='"+ nomEcole +"'";
+			st.executeUpdate(sql);
+		}
+		catch (SQLException e ) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				cn.close();
+				st.close();
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void updateEcole(String nomEcole)
+	{
+		String url = "jdbc:mysql://localhost/formation";
+		String login = "root";
+		String passwd = "";
+		java.sql.Connection cn = null;
+		java.sql.Statement st = null;
+		java.sql.ResultSet rs = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			cn = DriverManager.getConnection(url, login, passwd);
+			st = cn.createStatement();
+			String sql = "update Ecole set nomEcole ='"+ nomEcole +"' where nomEcole = 'Paris'";
+			st.executeUpdate(sql);
+		}
+		catch (SQLException e ) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				cn.close();
+				st.close();
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
